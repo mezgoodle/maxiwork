1c09dba86398d405833b100917e4d19a659f0e0a
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-07-18 16:32:12 +0000

    Max 21 apply the database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
+import { Task } from './tasks/entities/task.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URI,
+      entities: [Task],
+      synchronize: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/tasks/entities/task.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, ObjectId, ObjectIdColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @ObjectIdColumn()
+  id: ObjectId;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  userId: string;
+
+  @Column({ default: false })
+  isFinished: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: null, nullable: true })
+  updatedAt: Date;
+}
