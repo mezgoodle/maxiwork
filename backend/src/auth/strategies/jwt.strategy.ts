a77fcccf557e7c074abcffd0e6f5c9db2bb6d425
Maksym Zavalniuk <mezgoodle@gmail.com>
2026-09-21 16:57:44 +0300

    MAX-64: implement JWT Strategy, JwtAuthGuard and CurrentUser decorator

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../../users/users.service';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey:
+        configService.get<string>('JWT_ACCESS_SECRET') ||
+        'default_access_secret',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    try {
+      const user = await this.usersService.findById(payload.sub);
+      return user;
+    } catch {
+      throw new UnauthorizedException('User no longer exists or invalid token');
+    }
+  }
+}
